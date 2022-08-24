7f19c7809499c9fbe9da820fd433e6a55ec878e5
s1tuation <110306735+s1tuation@users.noreply.github.com>
2022-08-24 01:17:03 -0700

    Add files via upload

---
+++ TickTackToe/TickTackToe/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TickTackToe
+{
+    class Program
+    {
+        static char[]Grid = { "1" ,"2", "3", "4", "5", "6", "7", "8", "9" };
+        static int Player = 1;
+        static int Choices;
+        static void Main(string[] args)
+        {
+            //do while loop
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Player 1  x || Player 2 O");
+                if (Player % 2 == O) ;
+                {
+                    Console.WriteLine("Player 2 is starting");
+                }
+                else
+                {
+                    Console.WriteLine("Player 1 is starting");
+                }
+                Board();
+
+            }
+        
+
+        }
+    }lklk
+}
